29b742fb3e688172825a0efcb9ad77a29a316829
TomGroombridge <groombridgetom@gmail.com>
2015-03-10 15:46:57 +0000

    testing new spree.rb

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -15,7 +15,7 @@ Spree.config do |config|
 
 
       #S3 configuration
-      if Rails.env.production? then
+
            #production. Store images on S3.
            # development will default to local storage
           attachment_config = {
@@ -26,19 +26,19 @@ Spree.config do |config|
           },
 
 
-          storage:        :s3,
-          s3_headers:     { "Cache-Control" => "max-age=31557600" },
-          s3_protocol:    "https",
-          bucket:         ENV["S3_BUCKET"],
+          # storage:        :s3,
+          # s3_headers:     { "Cache-Control" => "max-age=31557600" },
+          # s3_protocol:    "https",
+          # bucket:         ENV["S3_BUCKET"],
 
-          path:          ":rails_root/public/:class/:attachment/:id/:style/:basename.:extension",
-          default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
-          default_style: "product",
+          # path:          ":rails_root/public/:class/:attachment/:id/:style/:basename.:extension",
+          # default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
+          # default_style: "product",
           }
 
           attachment_config.each do |key, value|
                Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
           end
      end
-end
+
 Spree.user_class = "Spree::User"
