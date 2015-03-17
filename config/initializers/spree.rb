4c587ada256a426fc5bdb9c3b56f04c4796ecfde
TomGroombridge <groombridgetom@gmail.com>
2015-03-17 11:23:07 +0000

    tweaked the size of the blue diamond as well as adding a contact modal

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -21,7 +21,7 @@ Spree.config do |config|
           attachment_config = {
           s3_credentials: {
             access_key_id: ENV["S3_KEY"],
-            secret_access_key: ENV["S3_SECRET"],
+            secret_access_key:  	,
             bucket: ENV["S3_BUCKET"],
           },
 
